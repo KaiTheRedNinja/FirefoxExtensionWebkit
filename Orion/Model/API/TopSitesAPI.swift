4763569295f172813595d26cd2adb560f0bb61f6
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-03-23 21:21:58 +0800

    Add top sites API stub

---
+++ Orion/Model/API/TopSitesAPI.swift
@@ -0,0 +1,17 @@
+//
+//  TopSitesAPI.swift
+//  Orion
+//
+//  Created by Kai Quan Tay on 23/3/23.
+//
+
+import Foundation
+
+enum TopSitesAPI {
+    func addSiteVisit(url: URL) {
+    }
+
+    func getTopSites(number: Int) -> [URL] {
+        []
+    }
+}
