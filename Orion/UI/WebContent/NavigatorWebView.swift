a5a455520e94dcf0d48c389c2124b1bd19d5357c
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-03-28 16:51:26 +0800

    Move web delegate functions from PageViewController -> NavigatorWebView

--- Orion/UI/WebContent/NavigatorWebView.swift
+++ Orion/UI/WebContent/NavigatorWebView.swift
@@ -7,9 +7,15 @@
 
 import Cocoa
 import WebKit
+import ExtensionsModel
 
 class NavigatorWebView: WKWebView {
+    var mainWindow: WindowController?
+
     var webViewURLObserver: NSKeyValueObservation?
+
+    /// A dictionary mapping source URLs to destination URLs
+    var downloadURLs: [URL: URL] = [:]
 
     override init(frame frameRect: CGRect, configuration: WKWebViewConfiguration = .init()) {
         super.init(frame: frameRect, configuration: configuration)
@@ -17,6 +23,7 @@ class NavigatorWebView: WKWebView {
         // use the firefox UA to get the firefox addon page to work
         customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 13.2; rv:111.0) Gecko/20100101 Firefox/111.0"
         autoresizingMask = [.height, .width]
+        navigationDelegate = self
 
         // watch the address
         self.webViewURLObserver = observe(\.url) { [weak self] webView, change in
@@ -37,6 +44,79 @@ class NavigatorWebView: WKWebView {
          This method should not be called on the main thread as it may lead to UI unresponsiveness.
          */
         load(request)
+    }
+}
+
+// MARK: Navigation
+extension NavigatorWebView: WKNavigationDelegatePlus {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        guard let url = webView.url else { return }
+        mainWindow?.updateAddressBar(to: url, title: self.title ?? "")
+    }
+
+    func webView(_ webView: WKWebView, urlChange: NSKeyValueObservedChange<URL?>) {
+        if let wkURL = webView.url {
+            mainWindow?.updateAddressBar(to: wkURL, title: self.title ?? "")
+        } else {
+            print("Could not get URL for page")
+        }
+    }
+}
+
+// MARK: Downloads
+extension NavigatorWebView: WKDownloadDelegate {
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationAction: WKNavigationAction,
+                 preferences: WKWebpagePreferences,
+                 decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void) {
+        if navigationAction.shouldPerformDownload {
+            decisionHandler(.download, preferences)
+        } else {
+            decisionHandler(.allow, preferences)
+        }
+    }
+
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationResponse: WKNavigationResponse,
+                 decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        if navigationResponse.canShowMIMEType {
+            decisionHandler(.allow)
+        } else {
+            decisionHandler(.download)
+        }
+    }
+
+    func webView(_ webView: WKWebView, navigationAction: WKNavigationAction, didBecome download: WKDownload) {
+        // TODO: Manage if the download is already downloaded
+        download.delegate = self
+    }
+
+    func webView(_ webView: WKWebView, navigationResponse: WKNavigationResponse, didBecome download: WKDownload) {
+        download.delegate = self
+    }
+
+    func download(_ download: WKDownload,
+                  decideDestinationUsing response: URLResponse,
+                  suggestedFilename: String,
+                  completionHandler: @escaping (URL?) -> Void) {
+        let fileManager = FileManager.default
+        var url = fileManager.getDocumentsDirectory().appendingPathComponent(suggestedFilename)
+        if suggestedFilename.hasSuffix(".xpi") {
+            url.deleteLastPathComponent()
+            url.append(component: "extensions/\(suggestedFilename)")
+        }
+        guard let sourceURL = download.originalRequest?.url else { return }
+        downloadURLs[sourceURL] = url
+        completionHandler(url)
+    }
+
+    func downloadDidFinish(_ download: WKDownload) {
+        guard let sourceURL = download.originalRequest?.url,
+              let destinationURL = downloadURLs[sourceURL] else {
+            return
+        }
+        downloadURLs.removeValue(forKey: sourceURL)
+        ExtensionManager.shared.loadXPI(url: destinationURL)
     }
 }
 
--- Orion/UI/WebContent/PageViewController.swift
+++ Orion/UI/WebContent/PageViewController.swift
@@ -11,7 +11,9 @@ import ExtensionsModel
 
 class PageViewController: NSViewController {
 
-    weak var mainWindow: WindowController?
+    weak var mainWindow: WindowController? {
+        didSet { wkView?.mainWindow = mainWindow }
+    }
 
     private var wkView: NavigatorWebView?
 
@@ -20,16 +22,12 @@ class PageViewController: NSViewController {
 
         // load the wkView
         let wkView = NavigatorWebView(frame: self.view.frame)
-        wkView.navigationDelegate = self
         self.view = wkView
         self.wkView = wkView
 
         // load contents
         wkView.loadPage(string: "https://addons.mozilla.org/en-US/firefox/addon/top-sites-button/")
     }
-
-    /// A dictionary mapping source URLs to destination URLs
-    var downloadURLs: [URL: URL] = [:]
 
     // MARK: Public-facing functions
 
@@ -43,76 +41,5 @@ class PageViewController: NSViewController {
 
     func loadPage(string: String) {
         wkView?.loadPage(string: string)
-    }
-}
-
-extension PageViewController: WKNavigationDelegatePlus, WKDownloadDelegate {
-    // MARK: Navigation
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        guard let url = webView.url else { return }
-        mainWindow?.updateAddressBar(to: url, title: wkView?.title ?? "")
-    }
-
-    func webView(_ webView: WKWebView, urlChange: NSKeyValueObservedChange<URL?>) {
-        if let wkURL = webView.url {
-            mainWindow?.updateAddressBar(to: wkURL, title: wkView?.title ?? "")
-        } else {
-            print("Could not get URL for page")
-        }
-    }
-
-    // MARK: Downloads
-    func webView(_ webView: WKWebView,
-                 decidePolicyFor navigationAction: WKNavigationAction,
-                 preferences: WKWebpagePreferences,
-                 decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void) {
-        if navigationAction.shouldPerformDownload {
-            decisionHandler(.download, preferences)
-        } else {
-            decisionHandler(.allow, preferences)
-        }
-    }
-
-    func webView(_ webView: WKWebView,
-                 decidePolicyFor navigationResponse: WKNavigationResponse,
-                 decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
-        if navigationResponse.canShowMIMEType {
-            decisionHandler(.allow)
-        } else {
-            decisionHandler(.download)
-        }
-    }
-
-    func webView(_ webView: WKWebView, navigationAction: WKNavigationAction, didBecome download: WKDownload) {
-        // TODO: Manage if the download is already downloaded
-        download.delegate = self
-    }
-
-    func webView(_ webView: WKWebView, navigationResponse: WKNavigationResponse, didBecome download: WKDownload) {
-        download.delegate = self
-    }
-
-    func download(_ download: WKDownload,
-                  decideDestinationUsing response: URLResponse,
-                  suggestedFilename: String,
-                  completionHandler: @escaping (URL?) -> Void) {
-        let fileManager = FileManager.default
-        var url = fileManager.getDocumentsDirectory().appendingPathComponent(suggestedFilename)
-        if suggestedFilename.hasSuffix(".xpi") {
-            url.deleteLastPathComponent()
-            url.append(component: "extensions/\(suggestedFilename)")
-        }
-        guard let sourceURL = download.originalRequest?.url else { return }
-        downloadURLs[sourceURL] = url
-        completionHandler(url)
-    }
-
-    func downloadDidFinish(_ download: WKDownload) {
-        guard let sourceURL = download.originalRequest?.url,
-              let destinationURL = downloadURLs[sourceURL] else {
-            return
-        }
-        downloadURLs.removeValue(forKey: sourceURL)
-        ExtensionManager.shared.loadXPI(url: destinationURL)
     }
 }
